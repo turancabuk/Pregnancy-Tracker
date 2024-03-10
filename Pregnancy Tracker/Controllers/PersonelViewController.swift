2d31f3ddcd0d975fee74ade4c5ef37bfaa9e96e6
Turan Çabuk <turan6677@gmail.com>
2024-03-10 16:40:55 +0300

    PersonalInformationView has been created.

---
+++ Pregnancy Tracker/Controllers/PersonelViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PersonelViewController.swift
+//  Pregnancy Tracker
+//
+//  Created by Turan Çabuk on 9.03.2024.
+//
+
+import UIKit
+
+class PersonelViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+}
---
+++ Pregnancy Tracker/Views/PersonalInformationView.swift
@@ -0,0 +1,21 @@
+//
+//  PersonalInformationView.swift
+//  Pregnancy Tracker
+//
+//  Created by Turan Çabuk on 10.03.2024.
+//
+
+import UIKit
+
+class PersonalInformationView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
