64e6fc7c19ae1d8531590c713aeb1399d932cafc
Turan Çabuk <turan6677@gmail.com>
2024-03-12 21:42:26 +0300

    PersonalInformationView has been completed.

--- Pregnancy Tracker/Views/PersonalInformationView.swift
+++ Pregnancy Tracker/Views/PersonalInformationView.swift
@@ -11,13 +11,17 @@ class PersonalInformationView: UIViewController, UIImagePickerControllerDelegate
     
     let personalCardColor = #colorLiteral(red: 0.970778048, green: 0.8382893801, blue: 0.8796723485, alpha: 1)
     let personalCardColor1 = #colorLiteral(red: 0.9507680535, green: 0.7077944875, blue: 0.8335040212, alpha: 1)
-    var pickerNumbers = Array(0...42)
-    var picker1Numbers = Array(0...6)
+    var pickerNumbers = Array(0...120)
+    var picker1Numbers = Array(0...99)
+    var picker3Numbers = Array(0...120)
+    var picker4Numbers = Array(0...99)
     let profileImageView = UIImageView()
     let nameTextfield = PaddedPlaceHolder()
     let datePicker = UIDatePicker()
     var picker = UIPickerView()
     var picker1 = UIPickerView()
+    var picker2 = UIPickerView()
+    var picker3 = UIPickerView()
 
     
     override func viewDidLoad() {
@@ -40,6 +44,31 @@ class PersonalInformationView: UIViewController, UIImagePickerControllerDelegate
         safeAreaView.layer.cornerRadius = 16
         safeAreaView.clipsToBounds = true
         
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.isUserInteractionEnabled = true
+        scrollView.isScrollEnabled = true
+        safeAreaView.addSubview(scrollView)
+
+        // Set scrollView constraints
+        scrollView.leadingAnchor.constraint(equalTo: safeAreaView.leadingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: safeAreaView.topAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: safeAreaView.trailingAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: safeAreaView.bottomAnchor).isActive = true
+
+        // Add a contentView to the scrollView
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.isUserInteractionEnabled = true
+        scrollView.addSubview(contentView)
+
+        // Set contentView constraints
+        contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        
         profileImageView.contentMode = .scaleAspectFill
         profileImageView.tintColor = .white
         profileImageView.layer.cornerRadius = 16
@@ -49,22 +78,23 @@ class PersonalInformationView: UIViewController, UIImagePickerControllerDelegate
         profileImageView.image = UIImage(systemName: "person.crop.circle.badge.plus")
         profileImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handlePicker)))
         
-        safeAreaView.addSubview(profileImageView)
-        profileImageView.centerXAnchor.constraint(equalTo: safeAreaView.centerXAnchor).isActive = true
+        contentView.addSubview(profileImageView)
+        profileImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         
         profileImageView.anchor(
-            top: view.topAnchor, leading: nil, bottom: nil, trailing: nil, padding: .init(
-                top: 80, left: 0, bottom: 0, right: 0), size: .init(width: 80, height: 80))
-        
+            top: contentView.topAnchor, leading: nil, bottom: nil, trailing: nil, padding: .init(
+                top: 20, left: 0, bottom: 0, right: 0), size: .init(width: 80, height: 80))
+
+
         let nameDesLabel = UILabel()
         nameDesLabel.text = "Select a photo"
         nameDesLabel.font = FontHelper.customFont(size: 15)
         nameDesLabel.textColor = .black
         nameDesLabel.textAlignment = .center
         
-        safeAreaView.addSubview(nameDesLabel)
+        contentView.addSubview(nameDesLabel)
         nameDesLabel.anchor(
-            top: profileImageView.bottomAnchor, leading: safeAreaView.leadingAnchor, bottom: nil, trailing: safeAreaView.trailingAnchor, padding: .init(
+            top: profileImageView.bottomAnchor, leading: contentView.leadingAnchor, bottom: nil, trailing: contentView.trailingAnchor, padding: .init(
                 top: 4, left: 60, bottom: 0, right: 60), size: .init(width: 120, height: 40))
         
         nameTextfield.placeHolderPadding = 18
@@ -80,10 +110,10 @@ class PersonalInformationView: UIViewController, UIImagePickerControllerDelegate
         nameTextfield.textColor = .purple
         nameTextfield.textAlignment = .left
         
-        safeAreaView.addSubview(nameTextfield)
+        contentView.addSubview(nameTextfield)
         nameTextfield.anchor(
-            top: nameDesLabel.bottomAnchor, leading: safeAreaView.leadingAnchor, bottom: nil, trailing: safeAreaView.trailingAnchor, padding: .init(
-                top: 36, left: 12, bottom: 0, right: 60), size: .init(width: safeAreaView.frame.width, height: 30))
+            top: nameDesLabel.bottomAnchor, leading: contentView.leadingAnchor, bottom: nil, trailing: contentView.trailingAnchor, padding: .init(
+                top: 36, left: 12, bottom: 0, right: 10), size: .init(width: contentView.frame.width, height: 30))
         
         let dateLabel = UILabel()
         dateLabel.text = "Select your Last Menstrual Period"
@@ -91,14 +121,14 @@ class PersonalInformationView: UIViewController, UIImagePickerControllerDelegate
         dateLabel.textColor = .purple
         dateLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        safeAreaView.addSubview(dateLabel)
+        contentView.addSubview(dateLabel)
         dateLabel.anchor(
-            top: nameTextfield.bottomAnchor, leading: safeAreaView.leadingAnchor, bottom: nil, trailing: nil, padding: .init(
-                top: 48, left: 30, bottom: 0, right: 0), size: .init(width: 280, height: 36))
+            top: nameTextfield.bottomAnchor, leading: contentView.leadingAnchor, bottom: nil, trailing: nil, padding: .init(
+                top: 36, left: 30, bottom: 0, right: 0), size: .init(width: 280, height: 36))
 
         let datePickerContainer = UIView()
         datePickerContainer.translatesAutoresizingMaskIntoConstraints = false
-        safeAreaView.addSubview(datePickerContainer)
+        contentView.addSubview(datePickerContainer)
         datePickerContainer.anchor(
             top: dateLabel.bottomAnchor, leading: dateLabel.leadingAnchor, bottom: nil, trailing: nil, padding: .init(
                 top: 6, left: 0, bottom: 0, right: 0), size: .init(width: 180, height: 40))
@@ -112,14 +142,13 @@ class PersonalInformationView: UIViewController, UIImagePickerControllerDelegate
         datePicker.addTarget(self, action: #selector(handleDatePicker), for: .valueChanged)
         datePickerContainer.addSubview(datePicker)
 
+        let weightLabel = UILabel()
+        weightLabel.text = "Your Weight"
+        weightLabel.textColor = .purple
+        weightLabel.font = FontHelper.customFont(size: 14)
         
-        let pregnancyWeekLabel = UILabel()
-        pregnancyWeekLabel.text = "or Select your Pregnancy Week"
-        pregnancyWeekLabel.font = FontHelper.customFont(size: 14)
-        pregnancyWeekLabel.textColor = .purple
-        
-        safeAreaView.addSubview(pregnancyWeekLabel)
-        pregnancyWeekLabel.anchor(
+        contentView.addSubview(weightLabel)
+        weightLabel.anchor(
             top: datePickerContainer.bottomAnchor, leading: datePickerContainer.leadingAnchor, bottom: nil, trailing: nil, padding: .init(
                 top: 36, left: 0, bottom: 0, right: 0), size: .init(width: 280, height: 40))
         
@@ -128,20 +157,51 @@ class PersonalInformationView: UIViewController, UIImagePickerControllerDelegate
         picker.delegate = self
         picker.dataSource = self
         
-        safeAreaView.addSubview(picker)
+        contentView.addSubview(picker)
         picker.anchor(
-            top: pregnancyWeekLabel.bottomAnchor, leading: safeAreaView.leadingAnchor, bottom: nil, trailing: nil, padding: .init(
-                top: 0, left: 4, bottom: 0, right: 0), size: .init(width: 180, height: 100))
+            top: weightLabel.bottomAnchor, leading: contentView.leadingAnchor, bottom: nil, trailing: nil, padding: .init(
+                top: 0, left: 10, bottom: 0, right: 0), size: .init(width: 140, height: 100))
         
         picker1.backgroundColor = .clear
         picker1.setValue(UIColor.black, forKey: "textColor")
         picker1.delegate = self
         picker1.dataSource = self
         
-        safeAreaView.addSubview(picker1)
+        contentView.addSubview(picker1)
         picker1.anchor(
-            top: pregnancyWeekLabel.bottomAnchor, leading: picker.trailingAnchor, bottom: nil, trailing: nil, padding: .init(
-                top: 0, left: 4, bottom: 0, right: 0), size: .init(width: 180, height: 100))
+            top: weightLabel.bottomAnchor, leading: picker.trailingAnchor, bottom: nil, trailing: nil, padding: .init(
+                top: 0, left: 2, bottom: 0, right: 0), size: .init(width: 140, height: 100))
+        
+        let heightLabel = UILabel()
+        heightLabel.text = "Your Height"
+        heightLabel.textColor = .purple
+        heightLabel.font = FontHelper.customFont(size: 14)
+        
+        contentView.addSubview(heightLabel)
+        heightLabel.anchor(
+            top: picker.bottomAnchor, leading: datePickerContainer.leadingAnchor, bottom: nil, trailing: nil, padding: .init(
+                top: 12, left: 0, bottom: 0, right: 0), size: .init(width: 280, height: 40))
+        
+        picker2.backgroundColor = .clear
+        picker2.setValue(UIColor.black, forKey: "textColor")
+        picker2.delegate = self
+        picker2.dataSource = self
+        
+        contentView.addSubview(picker2)
+        picker2.anchor(
+            top: heightLabel.bottomAnchor, leading: contentView.leadingAnchor, bottom: nil, trailing: nil, padding: .init(
+                top: 0, left: 2, bottom: 0, right: 0), size: .init(width: 140, height: 100))
+        
+        picker3.backgroundColor = .clear
+        picker3.setValue(UIColor.black, forKey: "textColor")
+        picker3.delegate = self
+        picker3.dataSource = self
+        
+        contentView.addSubview(picker3)
+        picker3.anchor(
+            top: heightLabel.bottomAnchor, leading: picker.trailingAnchor, bottom: nil, trailing: nil, padding: .init(
+                top: 0, left: 10, bottom: 0, right: 0), size: .init(width: 140, height: 100))
+
         
         let saveButton = UIButton(type: .system)
         saveButton.setTitle("Save", for: .normal)
@@ -153,11 +213,15 @@ class PersonalInformationView: UIViewController, UIImagePickerControllerDelegate
         saveButton.translatesAutoresizingMaskIntoConstraints = false
         saveButton.addTarget(self, action: #selector(handleSave), for: .touchUpInside)
         
-        safeAreaView.addSubview(saveButton)
-        saveButton.centerXAnchor.constraint(equalTo: safeAreaView.centerXAnchor).isActive = true
+        contentView.addSubview(saveButton)
+        saveButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         saveButton.anchor(
-            top: nil, leading: nil, bottom: safeAreaView.bottomAnchor, trailing: nil, padding: .init(
-                top: 0, left: 40, bottom: 26, right: 40), size: .init(width: 160, height: 40))
+            top: picker2.bottomAnchor, leading: nil, bottom: nil, trailing: nil, padding: .init(
+                top: 40, left: 40, bottom: 26, right: 40), size: .init(width: 160, height: 40))
+        
+        let endBottom = saveButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20) // Gerekli boşluk için sabiti ayarlayın
+        endBottom.isActive = true
+        endBottom.priority = UILayoutPriority(999)
     }
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -202,8 +266,8 @@ class PersonalInformationView: UIViewController, UIImagePickerControllerDelegate
         defaults.setValue(name, forKey: "userName")
         defaults.setValue(pickerNumbers[picker.selectedRow(inComponent: 0)], forKey: "weekValue")
         defaults.setValue(picker1Numbers[picker1.selectedRow(inComponent: 0)], forKey: "weekValue")
-        let selectedDate = datePicker.date
-        defaults.setValue(selectedDate, forKey: "lastMenstrualPeriod")
+
+        
         if let imageData = profileImage.jpegData(compressionQuality: 1.0) {
             defaults.set(imageData, forKey: "profileImage")
         }else{
