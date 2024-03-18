432b480be107b565d0bfeb29ea6dcce96a199797
Turan Çabuk <turan6677@gmail.com>
2024-03-18 20:10:27 +0300

    VerticalCollectionView has been configured.

--- Pregnancy Tracker/Controllers/Cells/VerticalCollectionViewCell.swift
+++ Pregnancy Tracker/Controllers/Cells/VerticalCollectionViewCell.swift
@@ -11,20 +11,47 @@ class VerticalCollectionViewCell: UICollectionViewCell {
     
     let containerView = UIView()
     let imageView = UIImageView()
+    let infoLabel = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         addSubview(containerView)
-        containerView.fillSuperview()
-        
         containerView.addSubview(imageView)
-        imageView.fillSuperview()
+        containerView.addSubview(infoLabel)
+        
+        containerView.backgroundColor = .white
+        infoLabel.textColor = .black
+        infoLabel.numberOfLines = 0
+        
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        infoLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        containerView.layer.cornerRadius = 12
+        containerView.layer.masksToBounds = true
         
         imageView.layer.cornerRadius = 12
         imageView.layer.masksToBounds = true
         imageView.contentMode = .scaleAspectFill
-        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            containerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            imageView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 12),
+            imageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 12),
+            imageView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -12),
+            imageView.widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 0.3),
+            
+            infoLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 12),
+            infoLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 12),
+            infoLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -4),
+            infoLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -12)
+        ])
+        ShadowLayer.setShadow(view: imageView, color: .darkGray, opacity: 1.0, offset: .init(width: 0.5, height: 0.5), radius: 5)
         ShadowLayer.setShadow(view: containerView, color: .darkGray, opacity: 1.0, offset: .init(width: 0.5, height: 0.5), radius: 5)
     }
     
