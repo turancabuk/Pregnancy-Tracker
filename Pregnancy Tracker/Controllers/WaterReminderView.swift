5b9ebf8bd819674294985fc1cbffcf4b087fd32a
Turan Çabuk <turan6677@gmail.com>
2024-05-04 15:36:37 +0300

    headerCategories/WaterReminderView has been created with SwiftUI

---
+++ Pregnancy Tracker/Controllers/WaterReminderView.swift
@@ -0,0 +1,18 @@
+//
+//  WaterReminderView.swift
+//  Pregnancy Tracker
+//
+//  Created by Turan Çabuk on 4.05.2024.
+//
+
+import SwiftUI
+
+struct WaterReminderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WaterReminderView()
+}
