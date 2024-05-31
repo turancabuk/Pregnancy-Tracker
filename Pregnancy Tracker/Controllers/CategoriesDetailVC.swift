4e9b4994582e96e32cb843c00040ad981f486442
turan cabuk <turan6677@gmail.com>
2024-05-31 18:09:10 +0300

    CategoriesDetailVC has been synchronized.

--- Pregnancy Tracker/Controllers/CategoriesDetailVC.swift
+++ Pregnancy Tracker/Controllers/CategoriesDetailVC.swift
@@ -30,16 +30,48 @@ class CategoriesDetailVC: UIViewController {
     func playVideo(at index: Int) {
         guard index < videoURLs.count, let url = URL(string: videoURLs[index])  else {return}
         
-        player = AVPlayer(url: url)
-        playerViewController = AVPlayerViewController()
-        playerViewController?.player = player
-        
-        if let playerVC = playerViewController {
-            addChild(playerVC)
-            view.addSubview(playerVC.view)
-            playerVC.view.frame = view.frame
-            playerVC.didMove(toParent: self)
-            player?.play()
+        DispatchQueue.global().async {
+            let asset = AVAsset(url: url)
+            let playerItem = AVPlayerItem(asset: asset)
+            
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else {return}
+                player = AVPlayer(url: url)
+                playerViewController = AVPlayerViewController()
+                playerViewController?.player = player
+                
+                if let playerVC = playerViewController {
+                    addChild(playerVC)
+                    view.addSubview(playerVC.view)
+                    playerVC.view.frame = view.frame
+                    playerVC.didMove(toParent: self)
+                    player?.play()
+                    
+                    NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying),
+                                                           name: .AVPlayerItemDidPlayToEndTime, object: player?.currentItem)
+                }
+            }
         }
+    }
+    @objc fileprivate func playerDidFinishPlaying(note: NSNotification) {
+        if let playerVC = playerViewController {
+            playerVC.willMove(toParent: nil)
+            playerVC.view.removeFromSuperview()
+            playerVC.removeFromParent()
+        }
+        player?.pause()
+        player = nil
+        playerViewController = nil
+        
+        currentVideoIndex += 1
+        if currentVideoIndex < videoURLs.count {
+            playVideo(at: currentVideoIndex)
+        }else{
+            print("All videos played")
+            self.dismiss(animated: true)
+        }
+    }
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 }
